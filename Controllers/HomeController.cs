899d2e3898e881019f328931040812f120e61f9d
jgrissom <jgrissom91@gmail.com>
2019-02-27 14:12:06 -0600

    Home Controller
    1) Add a Controllers folder
    2) Add the HomeController class to the Controllers folder
    3) Create a controller method using lambda expression

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Index() => "This is Home";
+    }
+}
