bab1adab54a49262fc3b03cb47cb38584b583bd5
jgrissom <jgrissom91@gmail.com>
2019-02-27 21:19:45 -0600

    Display Categories
    1) Create  Product Controller / Category controller method
    2) Use the repository to designate the list of Categories as the view’s model
    3) Add Views/Product/Category.cshml - iterate thru the model to display all categories

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class ProductController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private INorthwindRepository repository;
+        public ProductController(INorthwindRepository repo) => repository = repo;
+
+        public IActionResult Category() => View(repository.Categories);
+    }
+}
