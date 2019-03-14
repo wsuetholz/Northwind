f6597ec95b017ecba4de2d04c00831a0cfe00446
jgrissom <jgrissom91@gmail.com>
2019-03-13 22:44:33 -0500

    Customer Controller
    1) Create Customer Controller
    2) Create Customer/Register method
    3) Create Views/Customer/Register.cshtml

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
