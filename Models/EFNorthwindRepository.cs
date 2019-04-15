802cbbeaa6b63b9746e09fff0866466002abda91
jgrissom <jgrissom91@gmail.com>
2019-04-14 19:40:29 -0500

    Modify the Northwind data repository
    1) Modify the Northwind repository interface file - add the method signature for CustomerEdit
    2) Modify the Northwind repository class - implement the CustomerEdit class

--- Models/EFNorthwindRepository.cs
+++ Models/EFNorthwindRepository.cs
@@ -22,5 +22,18 @@ namespace Northwind.Models
             context.Customers.Add(customer);
             context.SaveChanges();
         }
+
+        public void EditCustomer(Customer customer)
+        {
+            var customerToUpdate = context.Customers.FirstOrDefault(c => c.CustomerID == customer.CustomerID);
+            customerToUpdate.Address = customer.Address;
+            customerToUpdate.City = customer.City;
+            customerToUpdate.Region = customer.Region;
+            customerToUpdate.PostalCode = customer.PostalCode;
+            customerToUpdate.Country = customer.Country;
+            customerToUpdate.Phone = customer.Phone;
+            customerToUpdate.Fax = customer.Fax;
+            context.SaveChanges();
+        }
     }
 }
