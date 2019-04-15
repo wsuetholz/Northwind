689932dbeaa9a93193ded78ab1aac37b036546d1
jgrissom <jgrissom91@gmail.com>
2019-04-14 19:02:20 -0500

    Create the Account controller
    1) Add the Login controller method and related view
    2) Add the AccessDenied controller method and related view

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login(string returnUrl)
+        {
+            // return url remembers the user's original request
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        public ViewResult AccessDenied() => View();
+    }
+}
