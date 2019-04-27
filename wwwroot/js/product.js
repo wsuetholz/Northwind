c725d35748e248d8cbd46973b21ac879efb25f38
jgrissom <jgrissom91@gmail.com>
2019-04-26 22:28:33 -0500

    Move javascript to external file
    1) Create a new .js file - product.js
    2) Cut and paste the existing javascript code into the new file
    3) Modify the script element in the Index view

---
+++ wwwroot/js/product.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    getProducts()
+
+    function getProducts() {
+        var id = $('#product_rows').data('id');
+        var discontinued = $('#Discontinued').prop('checked') ? "" : "/discontinued/false";
+        $.getJSON({
+            url: "../../api/category/" + id + "/product" + discontinued,
+            success: function (response, textStatus, jqXhr) {
+                //console.log(response);
+                $('#product_rows').html("");
+                for (var i = 0; i < response.length; i++){
+                    var css = response[i].discontinued ? " class=\"discontinued\"" : "";
+                    var row = "<tr" + css + ">"
+                        + "<td>" + response[i].productName + "</td>"
+                        + "<td class=\"text-right\">$" + response[i].unitPrice.toFixed(2) + "</td>"
+                        + "<td class=\"text-right\">" + response[i].unitsInStock + "</td>"
+                        + "</tr>";
+                    $('#product_rows').append(row);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                // log the error to the console
+                console.log("The following error occured: " + textStatus, errorThrown);
+            }
+        });
+    }
+
+    $('#CategoryId').on('change', function(){
+        $('#product_rows').data('id', $(this).val());
+        getProducts();
+    });
+
+    $('#Discontinued').on('change', function(){
+        getProducts();
+    });
+});
