dad7a3899c78cc849ced7d6d1fadcf39374a330f
jgrissom <jgrissom91@gmail.com>
2019-04-26 21:40:31 -0500

    Add an API method to filter products by discontinued true/false

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -18,5 +18,9 @@ namespace Northwind.Controllers
         [HttpGet, Route("api/product/{id}")]
         // returns specific product
         public Product Get(int id) => repository.Products.FirstOrDefault(p => p.ProductId == id);
+
+        [HttpGet, Route("api/product/discontinued/{discontinued}")]
+        // returns all products where discontinued = true/false
+        public IEnumerable<Product> GetDiscontinued(bool discontinued) => repository.Products.Where(p => p.Discontinued == discontinued).OrderBy(p => p.ProductName);
     }
 }
