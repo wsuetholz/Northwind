9d8e9629e24426622370663dad1df611385b9266
jgrissom <jgrissom91@gmail.com>
2019-04-26 21:38:46 -0500

    Create a new controller APIController
    1) Create a controller method to expose all products
    2) Define a custom route for the controller method: api/product

---
+++ Controllers/APIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class APIController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private INorthwindRepository repository;
+        public APIController(INorthwindRepository repo) => repository = repo;
+
+        [HttpGet, Route("api/product")]
+        // returns all products
+        public IEnumerable<Product> Get() => repository.Products.OrderBy(p => p.ProductName);
+    }
+}
